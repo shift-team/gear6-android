c51ce9019a3d12242aa673d0302d42fcf6c016a6
Tyler Winkler <tyler@tylerwinkler.com>
2019-03-04 01:44:22 -0500

    Socket connection has a shorter timeout

--- app/src/main/java/com/shift/gear6/adapters/WiFiAdapter.kt
+++ app/src/main/java/com/shift/gear6/adapters/WiFiAdapter.kt
@@ -3,18 +3,23 @@ package com.shift.gear6.adapters
 import com.shift.gear6.Config
 import java.io.InputStream
 import java.io.OutputStream
+import java.net.InetSocketAddress
 import java.net.Socket
+import java.net.SocketAddress
 
-class WiFiAdapter : IAdapter {
-    private var connection: Socket = Socket(
-                Config.WifiAdapter.ipAddress,
-                Config.WifiAdapter.port)
+class WiFiAdapter() : IAdapter {
+    private var mConnection = Socket()
+
+    init {
+        val address = InetSocketAddress(Config.WifiAdapter.ipAddress, Config.WifiAdapter.port)
+        mConnection.connect(address as SocketAddress, Config.WifiAdapter.timeOut)
+    }
 
     override fun getInputStream(): InputStream {
-        return connection.getInputStream()
+        return mConnection.getInputStream()
     }
 
     override fun getOutputStream(): OutputStream {
-        return connection.getOutputStream()
+        return mConnection.getOutputStream()
     }
 }
