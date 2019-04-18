daa4d29815fbcfc2e832c9bcef5286a2975da1c3
Tyler Winkler <tyler@tylerwinkler.com>
2019-04-18 00:49:26 -0400

    Changed images to have consistent sizes. Added a shadow to overlaying text. Added Retrofit HTTP library to build.gradle.

--- app/src/main/java/com/shift/gear6/activities/DriveViewerActivity.kt
+++ app/src/main/java/com/shift/gear6/activities/DriveViewerActivity.kt
@@ -2,23 +2,25 @@ package com.shift.gear6.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.TableRow
 import android.widget.TextView
-import com.shift.gear6.R
 import de.siegmar.fastcsv.reader.CsvReader
 import kotlinx.android.synthetic.main.activity_drive_viewer.*
 import java.io.File
 import java.io.FileReader
 
+
 class DriveViewerActivity : AppCompatActivity() {
+    var filename: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_drive_viewer)
+        setContentView(com.shift.gear6.R.layout.activity_drive_viewer)
 
-        var filename = intent.extras!!.getString("filename")!!
+        filename = intent.extras!!.getString("filename")!!
 
-        populateTable(filename)
+        populateTable(filename as String)
     }
 
     private fun populateTable(filename: String) {
@@ -48,5 +50,34 @@ class DriveViewerActivity : AppCompatActivity() {
                 newRow.addView(view)
             }
         }
+    }
+
+    fun onUploadButtonClicked(view: View) {
+        val protocol = "http"
+        val hostname = "192.168.0.2"
+        val port = 3000
+        val uploadPath = "obd2data"
+        val timeout = 5000 // five seconds
+
+        val url =
+            protocol + "://" +
+                    hostname + ":" +
+                    port.toString() + "/" +
+                    uploadPath
+
+
+        /*val uploadTask = UploadDataTask()
+        val params = UploadDataTask.Params()
+        params.filename = File(filesDir,filename).absolutePath
+
+        params.callback = {
+            if (it.success) {
+                Global.logMessage("Uploaded " + params.filename + " successfully")
+            } else {
+                Global.logMessage(it.error)
+            }
+        }
+
+        uploadTask.execute(params)*/
     }
 }
--- app/src/main/java/com/shift/gear6/tasks/server/UploadDataTask.kt
+++ app/src/main/java/com/shift/gear6/tasks/server/UploadDataTask.kt
@@ -1,20 +1,18 @@
 package com.shift.gear6.tasks.server
 
 import android.os.AsyncTask
-import com.shift.gear6.CarDataSnapshot
-import java.io.DataOutputStream
-import java.net.HttpURLConnection
 import java.net.URL
+
 
 class UploadDataTask : AsyncTask<UploadDataTask.Params, Unit, UploadDataTask.Result>() {
     class Params {
-        // TODO: Snapshot needs to be changed to a file upload
-        var snapshot: CarDataSnapshot? = null
+        var filename: String? = null
         var callback: ((Result) -> Unit)? = null
     }
 
     class Result {
         var success = true
+        var error = ""
     }
 
     private var myParams: Params? = null
@@ -27,47 +25,29 @@ class UploadDataTask : AsyncTask<UploadDataTask.Params, Unit, UploadDataTask.Res
 
     override fun doInBackground(vararg params: Params): Result {
         val result = Result()
+        myParams = params[0]
 
-        if (params.isEmpty() || params[0].snapshot == null) {
+        if (params.isEmpty()
+            || params[0].filename == null
+            || myParams!!.filename!!.isEmpty()
+        ) {
             result.success = false
+            result.error = "No filename provided"
 
             return result
         }
 
-        try {
-            myParams = params[0]
+        val url = URL(
+            protocol + "://" +
+                    hostname + ":" +
+                    port.toString() + "/" +
+                    uploadPath
+        )
 
-            val url = URL(
-                protocol + "://" +
-                        hostname + ":" +
-                        port.toString() + "/" +
-                        uploadPath
-            )
 
-            val conn = url.openConnection() as HttpURLConnection
-            conn.requestMethod = "POST"
-            conn.connectTimeout = timeout
-            conn.readTimeout = timeout
-            conn.doOutput = true
-            conn.doInput = true
 
-            /*val postData = params[0].snapshot?.toJSONString()?.toByteArray(StandardCharsets.UTF_8)
 
-            conn.setRequestProperty("charset", "utf-8")
-            conn.setRequestProperty("Content-length", postData?.size.toString())
-            conn.setRequestProperty("Content-Type", "application/json")*/
-
-            val outputStream = DataOutputStream(conn.outputStream)
-
-            //outputStream.write(postData)
-            outputStream.flush()
-
-            result.success = conn.responseCode == HttpURLConnection.HTTP_OK
-            return result
-        } catch (ex: Exception) {
-            result.success = false
-            return result
-        }
+        return result
     }
 
     override fun onPostExecute(result: Result) {
