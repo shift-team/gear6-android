b9f7a245cc462d9361c1ae3eee23a5b6cd1a01a8
Tyler Winkler <tyler@tylerwinkler.com>
2019-04-25 19:54:59 -0400

    Added a color overlay to touch events. Changed click method names to better conform to naming conventions

--- app/src/main/java/com/shift/gear6/activities/MainActivity.kt
+++ app/src/main/java/com/shift/gear6/activities/MainActivity.kt
@@ -1,19 +1,55 @@
 package com.shift.gear6.activities
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.ColorFilter
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MotionEvent
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import com.shift.gear6.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+    private class ImageTouchListener : View.OnTouchListener {
+        override fun onTouch(view: View?, event: MotionEvent?): Boolean {
+            if (view is ImageView) {
+                if (event?.action == MotionEvent.ACTION_DOWN) {
+                    (view as ImageView).setColorFilter(Color.GREEN, PorterDuff.Mode.OVERLAY)
+                } else if (event?.action == MotionEvent.ACTION_UP) {
+                    (view as ImageView).clearColorFilter()
+                }
+            }
+            return false
+        }
+    }
+
+    private class TextTouchListener(private var imageView: ImageView) : View.OnTouchListener {
+        override fun onTouch(view: View?, event: MotionEvent?): Boolean {
+            return imageView.dispatchTouchEvent(event)
+        }
+    }
+
+    private val imageTouchListener = ImageTouchListener()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageNewDrive.setOnTouchListener(imageTouchListener)
+        imagePreviousDrives.setOnTouchListener(imageTouchListener)
+        imageSettings.setOnTouchListener(imageTouchListener)
+
+        textNewDrive.setOnTouchListener(TextTouchListener(imageNewDrive))
+        textPreviousDrives.setOnTouchListener(TextTouchListener(imagePreviousDrives))
+        textSettings.setOnTouchListener(TextTouchListener(imageSettings))
     }
 
-    fun newDrive_onClick(view: View) {
+    fun onNewDriveClick(view: View) {
         val intent = Intent(this, SelectDataToFetchActivity::class.java)
 
         startActivity(intent)
@@ -25,9 +61,12 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    fun settings_onClick(view: View) {
+    fun onSettingsClick(view: View) {
         val intent = Intent(this, SettingsActivity::class.java)
 
         startActivity(intent)
+    }
+
+    fun onButtonTouch(view: View, event: MotionEvent) {
     }
 }
